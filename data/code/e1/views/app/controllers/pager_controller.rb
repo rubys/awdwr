1d0a24c56887b42eaddbbc2dfaef0a7f9664ecae
Sam Ruby <rubys@intertwingly.net>
2010-02-27 08:24:24 -0500

    Speed up the test by using a transaction

--- data/code/e1/views/app/controllers/pager_controller.rb
+++ data/code/e1/views/app/controllers/pager_controller.rb
@@ -11,34 +11,37 @@ class PagerController < ApplicationController
 
   def populate
     User.delete_all
-    ["Chris Pine",
-     "Chad Fowler",
-     "Dave Thomas",
-     "Andy Hunt",
-     "Adam Keys",
-     "Maik Schmidt",
-     "Mike Mason",
-     "Greg Wilson",
-     "Jeffrey Fredrick",
-     "James Gray",
-     "Daniel Berger",
-     "Eric Hodel",
-     "Brian Marick",
-     "Mike Gunderloy",
-     "Ryan Davis",
-     "Scott Davis",
-     "David Heinemeier Hansson",
-     "Scott Barron",
-     "Marcel Molina",
-     "Brian McCallister",
-     "Mike Clark",
-     "Esther Derby",
-     "Johanna Rothman",
-     "Juliet Thomas",
-     "Thomas Fuchs"].each {|name| User.create(:name => name)}
 
-    763.times do |i|
-      User.create(:name => "ZZUser #{"%03d" % i}")
+    User.transaction do
+      ["Chris Pine",
+       "Chad Fowler",
+       "Dave Thomas",
+       "Andy Hunt",
+       "Adam Keys",
+       "Maik Schmidt",
+       "Mike Mason",
+       "Greg Wilson",
+       "Jeffrey Fredrick",
+       "James Gray",
+       "Daniel Berger",
+       "Eric Hodel",
+       "Brian Marick",
+       "Mike Gunderloy",
+       "Ryan Davis",
+       "Scott Davis",
+       "David Heinemeier Hansson",
+       "Scott Barron",
+       "Marcel Molina",
+       "Brian McCallister",
+       "Mike Clark",
+       "Esther Derby",
+       "Johanna Rothman",
+       "Juliet Thomas",
+       "Thomas Fuchs"].each {|name| User.create(:name => name)}
+
+      763.times do |i|
+        User.create(:name => "ZZUser #{"%03d" % i}")
+      end
     end
   end
 
