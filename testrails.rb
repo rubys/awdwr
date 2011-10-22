b43e1c679bfb433c9efecebe2a25ce3632639dbc
Sam Ruby <rubys@intertwingly.net>
2011-10-22 13:35:21 -0400

    Don't add gorp to Gemfile

--- testrails.rb
+++ testrails.rb
@@ -147,6 +147,7 @@ Dir.chdir File.join(PROFILE.source,WORK) do
       ENV['RUBYLIB'].split(File::PATH_SEPARATOR).each do |path|
         path.sub! /\/lib$/, ''
         name = path.split(File::SEPARATOR).last
+        next if name == 'gorp'
         if File.exist?(File.join(path, "/#{name}.gemspec"))
           gemfile.puts "gem #{name.inspect}, :path => #{path.inspect}"
         end
