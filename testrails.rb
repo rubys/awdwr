f9e230feb80fd5f0e6f6f21ae71046d79bd12fd8
Sam Ruby <rubys@intertwingly.net>
2013-03-22 18:08:36 -0400

    backport relative_require to 1.8.7

--- testrails.rb
+++ testrails.rb
@@ -2,6 +2,15 @@
 require 'rubygems'
 require 'yaml'
 require 'ostruct'
+
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative 'clerk/rvm'
 require_relative 'clerk/rbenv'
 
