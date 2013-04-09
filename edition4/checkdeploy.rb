66123e9e731b0f6f02755fa03a5f442ce322b7e2
Sam Ruby <rubys@intertwingly.net>
2013-04-08 22:27:33 -0400

    start adding assertions about the deploy directory

--- edition4/checkdeploy.rb
+++ edition4/checkdeploy.rb
@@ -19,5 +19,24 @@ class DepotTest < Gorp::TestCase
     assert_select '.stderr', /Writing.*public\/assets\/rails-[0-9a-f]+\.png/
     assert_select '.stderr', /executing "touch.*restart.txt"/
     assert_select '.stderr', :text => /^failed/, :count => 0
+
+    # find deploy directory
+    config = File.read('depot/config/deploy.rb')
+    deploy_to  = config[/^set :deploy_to, "(.*)"/,1]
+    deploy_to.sub! '#{user}', config[/^set :user, '(.*)'/,1]
+    deploy_to.sub! '#{application}', config[/^set :application, '(.*)'/,1]
+    assert File.exist? deploy_to
+
+    # verify compressed css
+    css = Dir["#{deploy_to}/current/public/assets/application*.css"]
+    assert_equal 1, css.length
+    asset = File.read(css.first)
+    assert_match /\}\./, asset
+
+    # verify compressed js
+    js = Dir["#{deploy_to}/current/public/assets/application*.js"]
+    assert_equal 1, js.length
+    asset = File.read(js.first)
+    assert_match /\}function/, asset
   end
 end
