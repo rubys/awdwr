8460e668c8ae1076c5ca4c203891df1611bfd265
Sam Ruby <rubys@intertwingly.net>
2013-11-01 09:38:44 -0400

    parens are optional

--- environment.rb
+++ environment.rb
@@ -80,12 +80,12 @@ module AWDWR
           /^\s*"gem\s+'([-\w]+)'(,.*)"/
         ]
 
-        pattern = /GemfileEntry\.github\('([-\w]+)',\s*'([-\/\w]+)'/
+        pattern = /GemfileEntry\.github[ (]'([-\w]+)',\s*'([-\/\w]+)'/
         app_base.scan(pattern) do |gem, repos|
           libs << gem
         end
 
-        pattern = /GemfileEntry\.version\('([-\w]+)',\s*'([^']+)'/
+        pattern = /GemfileEntry\.version[ (]'([-\w]+)',\s*'([^']+)'/
         app_base.scan(pattern) do |gem, opts|
           gems << [gem, ', ' + opts.inspect]
         end
