c00bbf7973309c66b047173bd8d81fedac22b46c
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-25 09:46:09 +0530

    Fix RVM installation in Vagrant box
    - Made changes as per https://rvm.io/integration/vagrant.
    - Earlier it was failing in verifying gpg key.

--- vagrant/cookbooks/awdwr/recipes/rvm.rb
+++ vagrant/cookbooks/awdwr/recipes/rvm.rb
@@ -21,7 +21,8 @@ bash "install rvm" do
         cp -Lr /home/vagrant/.rvm/src/rvm /vagrant/cache/rvm-src
       fi
     else
-      curl -L https://get.rvm.io | bash -s stable
+      gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+      curl -sSL https://get.rvm.io | bash -s $1
 
       cp -Lr /home/vagrant/.rvm/src/rvm /vagrant/cache/rvm-src
       cp -Lr /home/vagrant/.rvm/archives /vagrant/cache/rvm-archives
