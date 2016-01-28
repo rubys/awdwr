e1fcf7b6a169b85cf8e03668c42eef270a2df046
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-28 15:15:18 +0530

    We still need this otherwise installing wunderbar fails
    Revert "No need to install ruby 1.9 and nokogiri packages"
    
    This reverts commit 3572a167f1b00eabae063e8ad0f70b7d242d8d1f.

--- vagrant/cookbooks/awdwr/recipes/depot.rb
+++ vagrant/cookbooks/awdwr/recipes/depot.rb
@@ -10,6 +10,8 @@
 package "nodejs"
 package "libmysqlclient-dev"
 package "mysql-server"
+package "ruby1.9.3"
+package "ruby-nokogiri"
 package "libpq-dev"
 package "zlib1g-dev"
 package "libgmp3-dev"
