5d6af8ff0f2a236ee9cf828d7c6c1bd6d13655bc
Sam Ruby <rubys@intertwingly.net>
2017-05-28 12:29:28 -0400

    errata

--- edition4/makedepot.rb
+++ edition4/makedepot.rb
@@ -2939,7 +2939,7 @@ class PayTypeSelector extends React.Component \{
     return (
       <div className="field">
         <label htmlFor="order_pay_type">Pay type</label>
-        <select id="pay_type' name="order[pay_type]">
+        <select id="pay_type" name="order[pay_type]">
           <option value="">Select a payment method</option>
           <option value="Check">Check</option>
           <option value="Credit card">Credit card</option>
