92d10d8452d81cd2f6598e73d6c453c6eb93b811
Dave Copeland <davetron5000@gmail.com>
2017-06-30 07:09:12 -0400

    use turbolinks:load

--- edition4/makedepot.rb
+++ edition4/makedepot.rb
@@ -2949,7 +2949,7 @@ section 13.1, 'Iteration H1: Webpacker and App-Like JavaScript' do
 import ReactDOM        from 'react-dom'           // <callout id="co.payment-details.pay_type.import-react-dom"/>
 import PayTypeSelector from 'PayTypeSelector'     // <callout id="co.payment-details.pay_type.import-PayTypeSelector"/>
 
-document.addEventListener('DOMContentLoaded', function() {       // <callout id="co.payment-details.pay_type.DOMContentLoaded"/>
+document.addEventListener('turbolinks:load', function() {       // <callout id="co.payment-details.pay_type.DOMContentLoaded"/>
   var element = document.getElementById(\"pay-type-component\"); // <callout id="co.payment-details.pay_type.getElementById"/>
   ReactDOM.render(<PayTypeSelector />, element);                 // <callout id="co.payment-details.pay_type.jsx"/>
 });
