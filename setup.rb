90626676688ab4edbc91abbf30d4526e37556cf5
Sam Ruby <rubys@intertwingly.net>
2019-03-17 20:41:21 -0400

    update to 2.6.1

--- setup.rb
+++ setup.rb
@@ -85,9 +85,8 @@ if `which rbenv`.empty?
     system 'bash -c "curl -L https://get.rvm.io | bash -s stable"'
     exit -1 unless File.exist? rvm_path
     cmd = "source #{rvm_path}/scripts/rvm; rvm default system; " +
-      "rvm --autolibs=enable requirements ruby-2.0.0"
+      "rvm --autolibs=enable requirements ruby-2.6.1"
     system 'bash -c ' + cmd.inspect
-    exit 0
   end
 end
 
