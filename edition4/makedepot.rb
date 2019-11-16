556e12271a9fbbd73009f8339345f5c1a3d96af7
Dave Copeland <davec@naildrivin5.com>
2019-11-16 12:51:57 -0500

    fix casing issue

--- edition4/makedepot.rb
+++ edition4/makedepot.rb
@@ -3311,11 +3311,11 @@ export default PurchaseOrderPayType
     # START: pay_type_params
       
     def pay_type_params
-      if order_params[:pay_type] == "Credit Card"
+      if order_params[:pay_type] == "Credit card"
         params.require(:order).permit(:credit_card_number, :expiration_date)
       elsif order_params[:pay_type] == "Check"
         params.require(:order).permit(:routing_number, :account_number)
-      elsif order_params[:pay_type] == "Purchase Order"
+      elsif order_params[:pay_type] == "Purchase order"
         params.require(:order).permit(:po_number)
       else
         {}
